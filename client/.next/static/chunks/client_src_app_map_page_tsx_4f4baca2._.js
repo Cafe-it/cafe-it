5ea05bb673a1b71189ac277e790adbdda8bcd5e7
CodyMan0 <hys83751952@gmail.com>
2025-07-04 15:16:57 +0900

    [FEAT] 기존 멀티 레포에 있던 코드 복사 및 모노레포에서 실행가능하도록 최상위 package.json 수정

---
+++ client/.next/static/chunks/client_src_app_map_page_tsx_4f4baca2._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/b65ad_framer-motion_dist_es_bf209dca._.js",
+  "static/chunks/728ee_motion-dom_dist_es_4a8f39c0._.js",
+  "static/chunks/8484d_tailwind-merge_dist_bundle-mjs_mjs_4d8e7d9e._.js",
+  "static/chunks/node_modules__pnpm_9d0b598e._.js",
+  "static/chunks/client_src_c72cb2a1._.js"
+],
+    source: "dynamic"
+});
